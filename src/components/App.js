992c2a371313f827277496e71ca9f76e4dfd4397
Amirabas Nasirzadeh <13.amirabas.nasirzadeh.86@gmail.com>
2025-02-11 16:03:11 +0330

    Finish screen was added

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import StartScreen from "./StartScreen";
 import Question from "./Question";
 import NextButton from "./NextButton";
 import Progress from "./Progress";
+import FinishScreen from "./FinishScreen";
 
 const initialState = {
   questions: [],
@@ -53,6 +54,8 @@ function reducer(state, action) {
         index: state.index + 1,
         answer: null,
       };
+    case "finish":
+      return { ...state, status: "finished" };
     default:
       throw new Error("Unknown action");
   }
@@ -99,8 +102,16 @@ export default function App() {
               answer={answer}
               dispatch={dispatch}
             />
-            <NextButton dispatch={dispatch} answer={answer} />
+            <NextButton
+              dispatch={dispatch}
+              answer={answer}
+              countQuestions={countQuestions}
+              index={index}
+            />
           </>
+        )}
+        {status === "finished" && (
+          <FinishScreen points={points} maxPosiblePoints={maxPosiblePoints} />
         )}
       </Main>
     </div>
---
+++ src/components/FinishScreen.js
@@ -0,0 +1,19 @@
+function FinishScreen({ points, maxPosiblePoints }) {
+  let emoji;
+  const precentage = (points / maxPosiblePoints) * 100;
+  if (precentage === 100) emoji = "🥇";
+  if (precentage >= 70 && precentage < 100) emoji = "🥈";
+  if (precentage >= 40 && precentage < 70) emoji = "🥉";
+  if (precentage >= 1 && precentage < 40) emoji = "❌";
+  if (precentage === 0) emoji = "🤬";
+
+  return (
+    <p className="result">
+      <span>{emoji}</span> You scored <strong>{points}</strong> out of{" "}
+      {maxPosiblePoints} ({Math.ceil(precentage)}
+      %)
+    </p>
+  );
+}
+
+export default FinishScreen;
